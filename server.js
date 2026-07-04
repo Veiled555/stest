a678a328a353a0b1204f25fcf4a6b5bf5ec29296
Veiled555 <gveiled53@gmail.com>
2026-07-04 13:50:20 +0900

    Refactor server setup and improve room management

--- server.js
+++ server.js
@@ -1,25 +1,78 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-  cors: { origin: "*" } // どこからのアクセスも許可する設定
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
 });
+
+const rooms = {};
 
 io.on('connection', (socket) => {
-  console.log('プレイヤーが接続しました：', socket.id);
 
-  socket.on('joinRoom', (roomCode) => {
-    socket.join(roomCode);
-    console.log(`部屋 [${roomCode}] に入室しました`);
-  });
+    socket.on('joinRoom', (roomCode) => {
+        socket.join(roomCode);
 
-  socket.on('sendFormula', (data) => {
-    socket.to(data.roomCode).emit('receiveFormula', data.formula);
-  });
+        if (!rooms[roomCode]) {
+            rooms[roomCode] = {
+                hostId: socket.id,
+                players: [socket.id]
+            };
+            socket.emit('roomJoined', { playerId: 1, isHost: true });
+        } else {
+            const room = rooms[roomCode];
+            if (room.players.length < 2) {
+                room.players.push(socket.id);
+                socket.emit('roomJoined', { playerId: 2, isHost: false });
+                
+                io.to(room.hostId).emit('requestTerrainSync');
+            } else {
+                socket.emit('roomFull');
+            }
+        }
+    });
+
+    socket.on('syncTerrain', (data) => {
+        const room = rooms[data.roomCode];
+        if (room) {
+            socket.to(data.roomCode).emit('receiveTerrain', {
+                terrain: data.terrain,
+                players: data.players
+            });
+        }
+    });
+
+    socket.on('sendFormula', (data) => {
+        socket.to(data.roomCode).emit('receiveFormula', data.formula);
+    });
+
+    socket.on('disconnect', () => {
+        for (const roomCode in rooms) {
+            const room = rooms[roomCode];
+            const index = room.players.indexOf(socket.id);
+            if (index !== -1) {
+                room.players.splice(index, 1);
+                if (room.players.length === 0) {
+                    delete rooms[roomCode];
+                } else if (room.hostId === socket.id) {
+                    room.hostId = room.players[0];
+                }
+                break;
+            }
+        }
+    });
 });
 
-// Render.comなどの環境に合わせたポート番号で起動する設定
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log('Server is running on port ' + PORT);
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
+
